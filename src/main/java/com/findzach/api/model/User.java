36a7bcd390d4185fb5b253e80f04e62e538d47a0
FindZach <zach@findzach.com>
2021-05-07 13:58:39 -0400

    Added user object
    closes #1

---
+++ src/main/java/com/findzach/api/model/User.java
@@ -0,0 +1,26 @@
+package com.findzach.api.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 5/7/2021
+ */
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstname;
+    private String lastname;
+
+    private String role;
+}
