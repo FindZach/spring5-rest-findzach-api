0092d2a7f863c15cdb462cb281824adc94e6dc86
FindZach <zach@findzach.com>
2021-05-07 14:11:47 -0400

    Implemented Bootstrap basics
    closes #8 #6

---
+++ src/main/java/com/findzach/api/bootstrap/Bootstrap.java
@@ -0,0 +1,23 @@
+package com.findzach.api.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 5/7/2021
+ */
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+
+    /**
+     * Will be called when application is ran
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
