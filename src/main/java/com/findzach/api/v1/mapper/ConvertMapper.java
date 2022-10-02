680de70a961960e5df33954fa9a3c6c56f94f590
FindZach <zach@findzach.com>
2022-10-02 11:13:16 -0400

    Added ConvertMapper.java to add more reusability of mapping for future objects.

--- src/main/java/com/findzach/api/v1/mapper/ConvertMapper.java
+++ src/main/java/com/findzach/api/v1/mapper/ConvertMapper.java
@@ -2,14 +2,15 @@ package com.findzach.api.v1.mapper;
 
 import com.findzach.api.domain.Tutorial;
 import com.findzach.api.v1.model.tutorial.TutorialDTO;
-import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 /**
  * @author Zach <zach@findzach.com>
- * @since 9/24/2022
+ * @since 10/2/2022
  */
-@Mapper
-public interface TutorialMapper extends ConvertMapper<TutorialDTO, Tutorial> {
-    TutorialMapper INSTANCE = Mappers.getMapper(TutorialMapper.class);
+public interface ConvertMapper<DTO, POJO> {
+
+    DTO pojoToDTO(POJO pojo);
+
+    POJO DTOtoPojo(DTO dto);
 }
