a57d50745f30a1e94bc4bf2eea33cf11809b1ad5
FindZach <zach@findzach.com>
2022-10-02 10:58:42 -0400

    Adjusting pathing on backend API, this was needed for Spring Security to properly work using withCredentials

--- src/main/java/com/findzach/api/security/login/security/WebSecurityConfig.java
+++ src/main/java/com/findzach/api/security/login/security/WebSecurityConfig.java
@@ -60,18 +60,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .antMatchers("/tutorials/**").permitAll()
-                .antMatchers("/tutorial/**").permitAll()
+                .antMatchers("/api/tutorial/**").permitAll()
+                .antMatchers("/api/tutorials/**").permitAll()
                 .antMatchers("h2ConsolePath" + "/**").permitAll()
                 .anyRequest().authenticated();
 
         // fix H2 database console: Refused to display ' in a frame because it set 'X-Frame-Options' to 'deny'
-        http.headers().frameOptions().sameOrigin();
+       // http.headers().frameOptions().sameOrigin();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
