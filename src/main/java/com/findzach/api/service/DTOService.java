beaf6050a4642541eed5dbc4e1c6615d1d794191
FindZach <zach@findzach.com>
2022-10-02 10:57:44 -0400

    DTOService to help with mapping and leveraging generics to prevent repeating code, not sure if I will stick with this; testing.

---
+++ src/main/java/com/findzach/api/service/DTOService.java
@@ -0,0 +1,25 @@
+package com.findzach.api.service;
+
+import com.findzach.api.domain.Tutorial;
+import com.findzach.api.v1.model.tutorial.TutorialDTO;
+
+import java.util.List;
+
+/**
+ * @author Zach <zach@findzach.com>
+ * @since 10/2/2022
+ */
+public interface DTOService<DTO, POJO> {
+
+
+    List<DTO> getAll();
+
+    DTO transferToTutorial(POJO pojo);
+    DTO getPojoById(Long id);
+
+    DTO create(DTO dto);
+
+    DTO save(Long id, DTO dto);
+
+    DTO update(Long id, DTO dto);
+}
